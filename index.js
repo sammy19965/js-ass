6bb166c62f34196683f9b8c200d65f2000e5da15
sammy19965 <afuyesamuel1000@gmail.com>
2022-05-22 21:44:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let musician = 0;
+if(musician <= 0){
+    console.log("not a group")
+}
+
+else if (musician===1) {
+    console.log("solo")
+}
+else if (musician===2) {
+    console.log("duet")
+}
+else if (musician===3) {
+    console.log("trio")
+}
+else if (musician===4) {
+    console.log("quartet")
+}
+else if (musician > 4) {
+    console.log("this is a a large group")
+}
